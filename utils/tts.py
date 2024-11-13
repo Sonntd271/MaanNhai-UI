4a5dd63aa46f9ee443d2f2d27709bfeb92a71ea2
GemsLoveNLP <6438169421@student.chula.ac.th>
2024-11-13 12:23:12 +0700

    add: new tts.py

--- utils/tts.py
+++ utils/tts.py
@@ -2,6 +2,18 @@ from gtts import gTTS
 import os
 import playsound
 
+# the voiceline when open, close, turn off
+OPEN = "The curtain is opened"
+CLOSE = "The curtain is closed"
+OFF = "The curtain is turning off"
+# add more voicelines here...
+
+# language 
+LANG = "en"
+
+# enabling of the TTS system
+ENA = False
+
 def speak(phrase):
     """Speaks phrase based on given input.
 
@@ -16,7 +28,7 @@ def speak(phrase):
     """
 
     # Create gTTS object
-    tts = gTTS(text=phrase, lang='en', slow=False)
+    tts = gTTS(text=phrase, lang=LANG, slow=False)
     
     # Save the speech to a temporary file
     temp_file = 'temp_audio.mp3'
